7db23dbce348cda46ac25d6766ec02486bc10137
Ronmycode <67519456+Ronmycode@users.noreply.github.com>
2025-03-31 17:03:32 -0600

    add data object

---
+++ Destructuring_Objects_Arrays/Object.js
@@ -0,0 +1,57 @@
+const Employees = [
+  {
+    id: 1,
+    Name: "Cameron",
+    Title: "Director of Service",
+    skills: {
+      excel: "expert",
+      Sql: "beginer",
+      ClientSpace: "expert",
+    },
+    Team: ["Steven", "Krista", "Jim", "Ron"],
+  },
+  {
+    id: 2,
+    Name: "Jim",
+    Title: "Principal Consultant",
+    skills: {
+      excel: "expert",
+      Sql: "expert",
+      ClientSpace: "expert",
+    },
+    Team: ["Steven", "Krista", "Ron", "Cameron"],
+  },
+  {
+    id: 3,
+    Name: "Krista",
+    Title: "Sr. Client Space Product Consultant",
+    skills: {
+      excel: "beginer",
+      Sql: "beginer",
+      ClientSpace: "expert",
+    },
+    Team: ["Steven", "Jim", "Ron", "Cameron"],
+  },
+  {
+    id: 4,
+    Name: "Steven",
+    Title: "Product Consultant",
+    skills: {
+      excel: "beginer",
+      Sql: "beginer",
+      ClientSpace: "expert",
+    },
+    Team: ["Krista", "Jim", "Ron"],
+  },
+  {
+    id: 5,
+    Name: "Ron",
+    Title: "Director of Service",
+    skills: {
+      excel: "beginer",
+      Sql: "beginer",
+      ClientSpace: "beginer",
+    },
+    Team: ["Steven", "Krista", "Jim"],
+  },
+];
